9cdd17937eaa52cc72c1de33e04c83b90161cc28
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-02-12 17:08:29 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -18,7 +18,13 @@ const CreatePost = () => {
     const [loading, setLoading] = useState(false)
 
     return (
-        <section className="max-w-7xl mx-auto"></section>
+        <section className="max-w-7xl mx-auto">
+            <div>
+                <h1 className='font-extrabold text-[#222328] text-[32px]'>Create</h1>
+                <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>Create imaginative and visually stunning  images through DALL-E AI and share them with community</p>
+            </div>
+
+        </section>
     )
 }
 
