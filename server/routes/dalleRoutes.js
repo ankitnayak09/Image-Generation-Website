965a2c9c2fce2900c14846d0b884e06119e3a95e
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-06-21 09:55:40 +0530

    Update Response Header

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -28,9 +28,11 @@ router.route("/").post(async (req, res) => {
 
 		const image = aiResponse.data.data[0].b64_json;
 
+		res.setHeader("Access-Control-Allow-Origin", "*");
 		res.status(200).json({ photo: image });
 	} catch (error) {
 		console.log(error);
+		res.setHeader("Access-Control-Allow-Origin", "*");
 		res.status(500).send(error?.response.data.error.message);
 	}
 });
